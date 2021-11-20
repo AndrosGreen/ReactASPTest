fa6f422030c324d92d9a4f8f156b6976e64ccc7b
AndrosGreen <gclandres@gmail.com>
2021-11-20 12:56:40 -0600

    main page changed

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -7,7 +7,7 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <p>Welcome to OUR ASP ITSUR TEST PAGE 👀👍</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
